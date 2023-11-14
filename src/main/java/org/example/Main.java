97bb949edae0d2ca63b623884600dbe1cca8ccab
Maria Puente <mariapuentebenito93@hotmail.com>
2023-11-14 23:36:30 +0100

    Realizado menu y finalizado el proyecto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,10 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Scanner;
 
 
 import org.example.conexion.GestorDB;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 public class Main {
     public static void main(String[] args) {
